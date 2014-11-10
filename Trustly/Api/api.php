fe50849dba0ae71bee5b430f64d593bd39dc3d13
Per Lejontand <per.lejontand@trustly.com>
2014-11-10 16:06:18 +0100

    Get all the openssl error strings we know for now and report back a proper error string when we get an openssl certificate error.

--- Trustly/Api/api.php
+++ Trustly/Api/api.php
@@ -243,7 +243,49 @@ abstract class Trustly_Api {
 		if($this->api_is_https) {
 			$ssl_result = curl_getinfo($curl, CURLINFO_SSL_VERIFYRESULT);
 			if($ssl_result !== 0) {
-				$error = 'Failed to connect to the Trusly API. SSL Verification error ' . $ssl_result;
+
+				$curl_x509_errors = array(
+					'0' => 'X509_V_OK',
+					'2' => 'X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT',
+					'3' => 'X509_V_ERR_UNABLE_TO_GET_CRL',
+					'4' => 'X509_V_ERR_UNABLE_TO_DECRYPT_CERT_SIGNATURE',
+					'5' => 'X509_V_ERR_UNABLE_TO_DECRYPT_CRL_SIGNATURE',
+					'6' => 'X509_V_ERR_UNABLE_TO_DECODE_ISSUER_PUBLIC_KEY',
+					'7' => 'X509_V_ERR_CERT_SIGNATURE_FAILURE',
+					'8' => 'X509_V_ERR_CRL_SIGNATURE_FAILURE',
+					'9' => 'X509_V_ERR_CERT_NOT_YET_VALID',
+					'10' => 'X509_V_ERR_CERT_HAS_EXPIRED',
+					'11' => 'X509_V_ERR_CRL_NOT_YET_VALID',
+					'12' => 'X509_V_ERR_CRL_HAS_EXPIRED',
+					'13' => 'X509_V_ERR_ERROR_IN_CERT_NOT_BEFORE_FIELD',
+					'14' => 'X509_V_ERR_ERROR_IN_CERT_NOT_AFTER_FIELD',
+					'15' => 'X509_V_ERR_ERROR_IN_CRL_LAST_UPDATE_FIELD',
+					'16' => 'X509_V_ERR_ERROR_IN_CRL_NEXT_UPDATE_FIELD',
+					'17' => 'X509_V_ERR_OUT_OF_MEM',
+					'18' => 'X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT',
+					'19' => 'X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN',
+					'20' => 'X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY',
+					'21' => 'X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE',
+					'22' => 'X509_V_ERR_CERT_CHAIN_TOO_LONG',
+					'23' => 'X509_V_ERR_CERT_REVOKED',
+					'24' => 'X509_V_ERR_INVALID_CA',
+					'25' => 'X509_V_ERR_PATH_LENGTH_EXCEEDED',
+					'26' => 'X509_V_ERR_INVALID_PURPOSE',
+					'27' => 'X509_V_ERR_CERT_UNTRUSTED',
+					'28' => 'X509_V_ERR_CERT_REJECTED',
+					'29' => 'X509_V_ERR_SUBJECT_ISSUER_MISMATCH',
+					'30' => 'X509_V_ERR_AKID_SKID_MISMATCH',
+					'31' => 'X509_V_ERR_AKID_ISSUER_SERIAL_MISMATCH',
+					'32' => 'X509_V_ERR_KEYUSAGE_NO_CERTSIGN',
+					'50' => 'X509_V_ERR_APPLICATION_VERIFICATION'
+				);
+
+				$ssl_error_str = null;
+				if(isset($curl_x509_errors[$ssl_result])) {
+					$ssl_error_str = $curl_x509_errors[$ssl_result];
+				}
+
+				$error = 'Failed to connect to the Trusly API. SSL Verification error #' . $ssl_result . ($ssl_error_str?': ' . $ssl_error_str:'');
 				throw new Trustly_ConnectionException($error);
 			}
 		}
